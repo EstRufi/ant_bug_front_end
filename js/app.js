241b6398780134cbc4469460573aeb1910e7d7e8
EstRufi <estelarb2311@gmail.com>
2026-06-13 15:17:42 -0300

    enviando o js arrumado e organizado com o carrossel feito

---
+++ js/app.js
@@ -0,0 +1,20 @@
+'use strict'
+import { moverCarrossel } from "./carrossel.js";
+export async function buscarDadosApi(){
+    try {
+        const url = `https://api.tvmaze.com/shows`
+        const response = await fetch(url)
+
+        // O throw new Error é para utilizar o Erro 
+        if(!response.ok) throw new Error("requisição falhou");
+        const dados = await response.json()
+
+        return dados
+        
+    } catch (error) {
+        let alerta =alert("não consegui pegar nada")
+        return alerta
+    }
+}
+
+moverCarrossel()
--- js/carrossel.js
+++ js/carrossel.js
@@ -1,19 +1,4 @@
-export async function buscarDadosApi(){
-    try {
-        const url = `https://api.tvmaze.com/shows`
-        const response = await fetch(url)
-
-        // O throw new Error é para utilizar o Erro 
-        if(!response.ok) throw new Error("requisição falhou");
-        const dados = await response.json()
-
-        return dados
-        
-    } catch (error) {
-        let alerta =alert("não consegui pegar nada")
-        return alerta
-    }
-}
+import { buscarDadosApi } from "./app.js"
 
 function randomImagens(dados){
     const imgTodas = dados.length
@@ -21,6 +6,7 @@ function randomImagens(dados){
     if(imgTodas > 0){
         const random = Math.random() * imgTodas
         const randomImg = Math.floor(random)
+                    // Aqui ta assim por que o ramdon ta enviando o id para dados
         const imgSorteado = dados[randomImg]
 
         //caso precise mudar é aqui
@@ -32,6 +18,7 @@ function randomImagens(dados){
             return img
         }
         else{
+            // Aqui caso tenha uma foto null, ele ira pedir novamente pro ramdon pega outro id
             return randomImagens(dados)
         }
     }
@@ -57,24 +44,32 @@ async function criarFotosCarrossel(dados){
     ulCarrossel.appendChild(li)
 }
 
- async function moverCarrossel(){
+ export async function moverCarrossel(){
     const ulCarrossel = document.getElementById('carrossel')
     let dados = await buscarDadosApi()
 
+    // aqui é para acontecer de forma infinita
     setInterval( () => {
+        // ele ta falando se os filhos da ul tiver vazia pode prencher com as 10 images
+        // de uma vez, se não ira ser uma por vez
         if(ulCarrossel.children.length === 0){
 
-            for(let i = 0;i < 7;i++){
+            for(let i = 0;i < 10;i++){
                 criarFotosCarrossel(dados)
             }
         }
-
+        // Precisa dele aqui fora para quando der 10 imagens ele ainda gerar mais uma para passar pro lado
+        //então se quiser que seja infinito precisa dele aqui
         criarFotosCarrossel(dados)
-
+        
+        // aqui para ele se mover na velocidade 0,5segundos ao inicio ao fim
         ulCarrossel.style.transition = 'transform 0.5s ease-in-out'
+        // aqui é para se mover do eixo X para esquerda ou direita 230px. ( se tiver positivo ou negativo indica se vai para esquerda ou direita)
         ulCarrossel.style.transform = `translateX(-230px)`
 
+        // aqui é para acontecer somente quando o tempo indicado acontecer
         setTimeout(() => {
+            // aqui é para buscar la no html oq foi pedido no caso o . é para achar a classe expecifica e se for id use #
             const primeiroItem = ulCarrossel.querySelector('.listaCarrossel');
             if (primeiroItem) {
                 primeiroItem.remove(); // Apaga a foto que sumiu na esquerda
@@ -84,5 +79,3 @@ async function criarFotosCarrossel(dados){
         },500)
     },1800)
 }
-
-moverCarrossel()
